9736b9899cab3dd97e110896141d328bf5194d38
JinTak <jtak23@gmail.com>
2018-01-31 15:28:32 -0700

    added blog-preview and product-preview components

---
+++ front-end/node_modules/@angular/material/typings/esm5/menu/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public-api';
+export { MatMenuItemBase as ɵa19, _MatMenuItemMixinBase as ɵb19 } from './menu-item';
+export { MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd19, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc19 } from './menu-trigger';
