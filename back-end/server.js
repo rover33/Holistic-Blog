3920f9cd4bfa454b7c2e15520cdec13dde4634c4
rover33 <remy.j.pearlstone@gmail.com>
2018-01-31 14:36:34 -0700

    working

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 //Comment for testing 
 const express = require('express');
 const app = express();
+const path = require('path')
+require('dotenv').config()
 
 let PORT = process.env.PORT || 3000;
 
@@ -15,3 +17,16 @@ app.get('/', function(request, response){
 app.listen(PORT, function(){
 	console.log('Listening');
 });
+
+ //CORS setup to allow other ports from this host
+
+  //Only needed if not on Heroku/prod
+  if(!process.env.DYNO) {
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+      next();
+    });
+  }
