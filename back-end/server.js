569071314e4e42906d880141ea87da8c09bda0b5
miladabujarada <milad.abujarada@gmail.com>
2018-02-01 12:25:41 -0700

    not very important

--- back-end/server.js
+++ back-end/server.js
@@ -26,7 +26,7 @@ app.listen(PORT, function(){
   if(!process.env.DYNO) {
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
       next();
