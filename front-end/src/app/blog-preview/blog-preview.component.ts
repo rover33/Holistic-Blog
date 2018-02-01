1d1a2f9745ee5740805bfeb7c8ab7a816143032d
JinTak <jtak23@gmail.com>
2018-02-01 07:46:46 -0700

    more styling. added animation for home page

--- front-end/src/app/blog-preview/blog-preview.component.ts
+++ front-end/src/app/blog-preview/blog-preview.component.ts
@@ -9,19 +9,22 @@ export class BlogPreviewComponent implements OnInit {
 
   blogsArray = [
     {
-      title: "Blog ONE",
-      body: "This is the first blog!",
-      author: "Patrycja"
+      title: "Vitamine C is for COOL KIDS",
+      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque maxime molestiae sit facilis ab perspiciatis omnis, deserunt fugit debitis ratione, officiis quisquam. Debitis, commodi. Architecto distinctio possimus in maiores velit?" ,
+      author: "MILAD",
+      image: "https://d3r2zleywq7959.cloudfront.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/6/0/604926_xlarge.jpg"
     },
     {
-      title: "Blog TWO",
-      body: "This is the second blog!",
-      author: "Patricia"
+      title: "HOLY SHIT THIS SHIT IS THE SHIT!",
+      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque maxime molestiae sit facilis ab perspiciatis omnis, deserunt fugit debitis ratione, officiis quisquam. Debitis, commodi. Architecto distinctio possimus in maiores velit?" ,
+      author: "JIN",
+      image: "http://media1.s-nbcnews.com/j/MSNBC/Components/Photo/_new/110406-viagra-5p.grid-6x2.jpg"
     },
     {
-      title: "Blog THREE",
-      body: "This is the threeeee blog!",
-      author: "Patricia"
+      title: "YA BOI!!!!!",
+      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque maxime molestiae sit facilis ab perspiciatis omnis, deserunt fugit debitis ratione, officiis quisquam. Debitis, commodi. Architecto distinctio possimus in maiores velit?" ,
+      author: "STEVANO",
+      image: "http://cdn.shopify.com/s/files/1/1525/0640/products/1500_grande.png?v=1507046871"
     }
   ];
 
--- front-end/src/app/product-preview/product-preview.component.ts
+++ front-end/src/app/product-preview/product-preview.component.ts
@@ -9,19 +9,22 @@ export class ProductPreviewComponent implements OnInit {
 
   productsArray = [
     {
-      title: "PROD ONE",
+      title: "blahh",
       body: "This is the first PRODUCTS!",
-      author: "MILAD"
+      author: "MILAD",
+      image: "https://d3r2zleywq7959.cloudfront.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/6/0/604926_xlarge.jpg"
     },
     {
       title: "PROD TWO",
       body: "This is the second PRODUCTS!",
-      author: "REMY"
+      author: "REMY",
+      image: "http://media1.s-nbcnews.com/j/MSNBC/Components/Photo/_new/110406-viagra-5p.grid-6x2.jpg"
     },
     {
       title: "PROD THREE",
       body: "This is the threeeee PRODUCTS!",
-      author: "STEVANO"
+      author: "STEVANO",
+      image: "http://cdn.shopify.com/s/files/1/1525/0640/products/1500_grande.png?v=1507046871"
     }
   ];
 
