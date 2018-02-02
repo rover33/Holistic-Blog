4e68aecdb7457e284138191933ae36a523b949df
rover33 <remy.j.pearlstone@gmail.com>
2018-02-01 17:41:45 -0700

    pushing

--- back-end/controllers/controllers.js
+++ back-end/controllers/controllers.js
@@ -14,13 +14,14 @@ let getBlogs = (response) => {
 };
 
 let getProducts = (response) => {
-/*	Product.create({
-		product_id: Math.ceil((Math.random() * 10000)),
-		name: 'product',
-		description: 'description of a product of products',
-		price: 25.99,
-		image_url: 'www.image.com'
-	});*/
+// Product.create({
+// 		product_id: Math.ceil((Math.random() * 10000)),
+// 		name: 'product',
+// 		description: 'description of a product of products',
+// 		price: 25.99,
+// 		image_url: 'www.image.com'
+// 	})
+	
 	Product.findAll().then(results => {
 		response.json(results);
 	});
