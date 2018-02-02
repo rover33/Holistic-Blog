68af5c6b8267cf4920ca0197ca997c3a7bfafd47
JinTak <jtak23@gmail.com>
2018-02-02 16:22:51 -0700

    styling login

--- back-end/controllers/controllers.js
+++ back-end/controllers/controllers.js
@@ -4,11 +4,11 @@ let Blog = models.Blog;
 let Product = models.Product;
 
 let getBlogs = (response) => {
-/*	blog = new Blog();
+	blog = new Blog();
 	blog.blogTitle = `random ${Math.random()}`;
 	blog.blogBody = `lorem Epsom ${Math.random()}`;
 	blog.date = Date();
-	blog.save();*/
+	blog.save();
 
 	Blog.find({}, null, {sort:{date: -1}}, (err, results) => {
 		response.json(results)});
@@ -18,20 +18,20 @@ let getBlog = (request, response) => {
 	Blog.findOne({_id: request.params.id}).then((result) => response.json(result));
 };
 
-// Product.create({
-// 		product_id: Math.ceil((Math.random() * 10000)),
-// 		name: 'product',
-// 		description: 'description of a product of products',
-// 		price: 18.29,
-// 		image_url: 'www.image.com'
-// 	});
+Product.create({
+		product_id: Math.ceil((Math.random() * 10000)),
+		name: 'product',
+		description: 'description of a product of products',
+		price: 18.29,
+		image_url: 'www.image.com'
+	})
 	Product.findAll().then(results => response.json(results));
-};
+
 
 let getProduct = (request, response) => {
 	Product.findOne({where: {product_id: request.params.id}}).then((result) => response.json(result));
 }
 module.exports.getBlogs = getBlogs;
 module.exports.getBlog = getBlog;
-module.exports.getProducts = getProducts;
+// module.exports.getProducts = getProducts;
 module.exports.getProduct = getProduct;
