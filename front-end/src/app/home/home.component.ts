9b7ca705735850d75f018951d475dc9d1d8fced6
JinTak <jtak23@gmail.com>
2018-02-05 12:24:01 -0700

    shopping cart service. add to cart. still need to test with back end

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -2,6 +2,7 @@
 import { ProductService } from './../services/product.service';
 import { BlogService } from './../services/blog.service';
 import { Component, OnInit } from '@angular/core';
+import { QueryValueType } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,8 @@ export class HomeComponent implements OnInit {
 
   threeBlogs = [];
   threeProducts = [];
+
+  itemQty : number
 
   constructor(
     private blogService : BlogService,
@@ -35,19 +38,37 @@ export class HomeComponent implements OnInit {
 
       this.productService.getAllProducts()
       .subscribe( response => {
-        // console.log(response);
+        console.log(response);
         // console.log("Successfully retreived BLOGS : ", response.json());
         this.threeProducts = response.json().splice(0, 3);
       })
   }
 
 
-  getOneBlog(blogId){
-    console.log(blogId);
-    this.blogService.getSingleBlog(blogId)
-      .subscribe( response => {
-        console.log(response);
+  // getOneBlog(blogId){
+  //   console.log(blogId);
+  //   this.blogService.getSingleBlog(blogId)
+  //     .subscribe( response => {
+  //       console.log(response);
         
+  //     })
+  // }
+
+
+  // This function will check availability of the Product before adding to the cart
+  addToCart(productID, i){
+    console.log('card',i);
+
+    let qty = (<HTMLInputElement>document.getElementById(`card-${i}`)).value;
+    
+    // console.log(`You are trying to buy ${qty} items`)
+    // console.log('product ID: ', productID);
+
+    this.productService.addToCart(productID, qty)
+      .subscribe( response => {
+        console.log('Successfully added to cart');
+        localStorage.setItem('productID', productID);
+        localStorage.setItem('quantity', qty);
       })
   }
 
