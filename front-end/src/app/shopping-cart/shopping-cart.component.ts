493e27beefc2ab144d3dc509a433cea68f87a16e
JinTak <jtak23@gmail.com>
2018-02-06 14:47:35 -0700

    fixed shopping cart.

--- front-end/src/app/shopping-cart/shopping-cart.component.ts
+++ front-end/src/app/shopping-cart/shopping-cart.component.ts
@@ -44,11 +44,15 @@ export class ShoppingCartComponent implements OnInit {
   purchase(){
 
     this.shoppingCartService.purchase();
-
-    alert('Order was placed!');
       // .subscribe( response => {
       //   console.log(response);
       // })
+    alert('Order was placed!');
     
+    localStorage.clear();
+    
+    this.currentCart = null;
+
+    // console.log(this.currentCart);
   }
 }
