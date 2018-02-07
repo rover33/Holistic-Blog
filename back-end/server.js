9d74bbcf6e79e98d6b0912d7d8d24e7d8047bfaf
rover33 <remy.j.pearlstone@gmail.com>
2018-02-06 18:51:53 -0700

    Heroku 7

--- back-end/server.js
+++ back-end/server.js
@@ -26,10 +26,10 @@ if(!process.env.DYNO) {
 
 app.use('/', routes);
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + 'dist'));
 });
 
 let PORT = process.env.PORT || 3000;
